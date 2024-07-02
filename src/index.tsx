ee99ab9f52dcfe688459c20b73b142c5efa50e3c
Sergey Akkuratov <sergey.akkuratov@gmail.com>
2024-07-02 20:31:08 +0300

    Init commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import { createRoot } from "react-dom/client";
+
+
+const root = createRoot(document.getElementById("app")!);
+root.render(
+    <>
+        <h1>Hello!</h1>
+    </>
+);
